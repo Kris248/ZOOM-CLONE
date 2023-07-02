e290aac6b12203f671dd3d3ee643bf1228ca72af
Kris248 <chris.gautam.0024@gmail.com>
2023-07-02 09:11:30 +0530

    ZOOM CLONE DONE

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Plan of Action
+
+- Initialize the NodeJS Project
+- Initialize our first view
+- Create a room id
+- Add Ability to view our own video.
+- Add Ability to allow others to stream their own video.
+- Add styling
+- Add Ability to create messages
+- Add mute button
+- Add Stop Video button
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express'); // require karo express ko
+const app = express() // app me express ki functionality dalo.
+const server = require('http').Server(app); // app ko server me dalo.
+const {v4: uuidv4} = require('uuid'); // for uniq ID
+const io = require('socket.io')(server);
+const {ExpressPeerServer} = require('peer')
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+})
+
+
+
+app.set('view engine', 'ejs'); // setting engine
+app.use(express.static('public')); // IMP to include public files.
+app.use('/peerjs', peerServer);
+
+
+app.get('/', (req,res)=>{ 
+  res.redirect(`/${uuidv4()}`);
+}) // create uniq id in URL
+
+app.get('/:room', (req,res)=>{
+  res.render('room', {roomId: req.params.room})
+})
+
+//establishing real-time connection
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId)=>{
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected', userId)// tellin certain user has connected to everyone
+
+    socket.on('message', message=>{ // recieve msg
+      io.to(roomId).emit('createMessage', message) //sending msg to same room
+    })
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId)
+    })
+
+  })
+})
+
+
+
+server.listen(3030);// server ko listen karo
+
+
